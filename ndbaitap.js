af167d753c6134744909387f2b14f35bbd35b680
Thanhpham1010 <110167022+Thanhpham1010@users.noreply.github.com>
2022-07-29 08:17:01 +0700

    Add files via upload

---
+++ ndbaitap.js
@@ -0,0 +1,139 @@
+// 1. trong javascript, trpng truong hop nao khi ban dat ten bien se bi thong bao SyntaxError?
+//    dat ten bien bat dau bang chu so.
+//    dat ten bien chua nhung ki tu dac biet ( khoang trang cung la ky tu dac biet).
+//    chua cac keyword cua javascript.
+//  Cho 3 vi du ve viec dat ten bien sai torng javascript?
+//   let 1max=....
+//   let max max=....
+//   let console=....
+// trong javascript lam the nao de kiem tra du lieu?
+
+// cobao nhieu kieu du lieu co ban trong jacascript,ke ten?
+//   co 7 kieu du lieu chinh (string,number, boolean, null, object, underfined, array)
+
+
+
+
+
+//2. gan gia tri cho bien
+// 2.a
+// let msg = " Rikkei is awesome";
+// console.log(msg);
+// 2.b
+// let studentcount = 0;
+// console.log(studentcount);
+// 
+
+
+
+// 3.gan lai gia tri cho cac bien o phia tren.
+// a. thay doi gia tri bien msg
+//  msg = "coding is hard but super cool and fun";
+//  console.log(msg);
+//  b.
+// studentcount = studentcount + 30;
+// console.log(studentcount);
+// c.Google search
+//   cách để biến một chuỗi thành viết hoa toàn bộ (upper case),
+// console.log(msg.toUpperCase);
+//   viết thường toàn bộ (lower case).
+// console.log.(msg.toLowerCase);
+//   Biến đổi chuỗi msg thành dạng viết hoa toàn bộ → In nó ra màn hình console.
+
+
+
+
+// d.Google search cách để tăng một biến kiểu số lên n đơn vị →
+// Thực hiện cho người dùng nhập vào số n,
+//  sau đó tăng biến studentCount lên n đơn vị và in nó ra màn hình console
+//  let n = prompt("nhap so n");
+ 
+//  studentcount = (studentcount +Number(n));
+//  console.log(studentcount);
+
+// 4. Google search về câu lệnh alert trong Javascript 
+// → Show cho người dùng một tin nhắn đáng yêu nào đó
+
+// Hàm alert() trong JavaScript được sử dụng để hiển thi hộp thoại cảnh báo 
+// khi muốn đảm bảo thông tin đến với người dùng. 
+// Khi hộp thoại cảnh báo hiện lên, người dùng sẽ phải nhấp vào nút “OK” để tiếp tục.
+// alert("BAN THAT DANG YEU");
+
+
+// 5.Viết một đoạn mã hỏi tên người dùng, sau đó chào họ và nói gì đó đáng yêu với họ.
+
+// let name = prompt("your name ?");
+// console.log(name + " beautyful");
+
+
+// 6.
+//  let firstname = prompt("first name?");
+//  let lastname = prompt("last name ?")
+//  console.log("xin chao " +lastname +" " + firstname );
+
+
+//  7.Viết một đoạn mã hỏi tên người dùng, sau đó chào họ và nói gì đó đáng yêu với họ
+//  let a = prompt(" nhap do dai canh hinh vuong a =");
+//  S = a*a;
+//  console.log("dthv la : "+ S);
+
+
+//  8. 
+// let r = prompt(" ban kinh hinh tron la r =");
+// Sht = 3.14*r*r;
+// console.log(" dien tich hinh tron la: "+ Sht);
+// alert(" dien tich hinh tron la Sht=  "+ Sht);
+
+
+
+
+// 9. 
+// let C = prompt (" Nhap do C =");
+// F = C*33.8;
+// console.log(C +" Celsius = " +F +" Fahrenheit");
+
+
+// 10.
+// alert("The quit ---- fox jumps over the ---- dog");
+// let x= prompt ("ban dien tu thu nhat la : ");
+// let y= prompt ('ban dien tu thu 2 la: ');
+// console.log("The quit "+ x +  " fox jumps owver the " + y+ " dog");
+
+
+
+
+
+
+// homework
+// BAI 1.
+let a = Number(prompt("diem vat ly : "));
+let b = Number(prompt("diem hoa hoc : "));
+let c = Number(prompt("diem sinh hoc : "));
+T = a+b+c;
+tb =  T/3;
+console.log("diem trung binh 3 mon la: " +tb); 
+console.log("diem tong 3 mon la: " +T); 
+alert("diem tong 3 mon la: "+ T);
+alert("diem trung binh 3 mon la: "+ tb);
+
+
+
+
+// BAI 2.
+// let C= prompt(" moi nhap oC");
+// F= 1.4*C +32;
+// console.log(C +" oC = "+ F +" oF");
+// alert(C +" oC = "+ F +" oF");
+
+
+
+
+// bai3 +bai 4 : tinh dien tich hinh tron., Chu vi hinh tron.
+// let r = prompt(" nhap ban kinh r= ");
+// Sht= 3.14*r*r;
+// console.log("dien tich hinh tron la: "+ Sht);
+// alert(" dien tich hinh tron la : "+ Sht);
+// Cht=3.14*2*r;
+// console.log("chu vi hinh tron la: "+ Cht);
+// alert(" chu vi hinh tron la : "+ Cht);
+
